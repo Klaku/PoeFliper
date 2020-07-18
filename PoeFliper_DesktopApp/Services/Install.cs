28aaa212cc3fb2fc3a1b8a7fe5f78477bb75d00f
Rafał <rafass1000@gmail.com>
2020-07-18 08:27:48 +0200

    File Download

--- PoeFliper_DesktopApp/Services/Install.cs
+++ PoeFliper_DesktopApp/Services/Install.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,7 +17,30 @@ namespace PoeFliper_DesktopApp.Services
                 Directory.CreateDirectory(path);
             }
 
+            Install.DownloadFile("\\tsconfig.json", path);
+            Install.DownloadFile("\\package-lock.json", path);
+            Install.DownloadFile("\\package.json", path);
+            if (!Directory.Exists(path+"\\dist"))
+            {
+                Directory.CreateDirectory(path + "\\dist");
+            }
+            Install.DownloadDistFile("\\core.js", path);
+        }
 
+        private static void DownloadFile(string fileName, string path)
+        {
+            using (WebClient client = new WebClient())
+            {
+                client.DownloadFile("https://raw.githubusercontent.com/Klaku/PoeFliper/master/PoeSniper_NodeApp" + fileName, path + fileName);
+            }
+        }
+
+        private static void DownloadDistFile(string fileName, string path)
+        {
+            using (WebClient client = new WebClient())
+            {
+                client.DownloadFile("https://raw.githubusercontent.com/Klaku/PoeFliper/master/PoeSniper_NodeApp/dist" + fileName, path + "\\dist" + fileName);
+            }
         }
     }
 }
